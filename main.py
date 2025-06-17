1765fb182ee64ad5e64c1b37e26af2219e9c7866
ffffahh <adilbekmk@icloud.com>
2025-06-17 14:47:35 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Привет! Я твой бот Zharna 🤖")
+
+bot.infinity_polling()
